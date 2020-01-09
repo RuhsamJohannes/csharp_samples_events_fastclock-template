66911c94c4847fc58f10abd00c20642d727d48fb
JohannesRuhsam <johannes.ruhsam@gmx.at>
2020-01-09 20:26:15 +0100

    FastClock implementiert

--- source/FastClock/EventsDemo.FastClock/FastClock.cs
+++ source/FastClock/EventsDemo.FastClock/FastClock.cs
@@ -5,5 +5,64 @@ namespace EventsDemo.FastClock
 {
     public class FastClock
     {
+        private static FastClock _instance;
+
+        public static FastClock Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new FastClock(DateTime.Now);
+                }
+
+                return _instance;
+            }
+        }
+
+        private readonly DispatcherTimer _timer;
+        private bool _isRunning;
+
+        public EventHandler<DateTime> OneMinuteIsOver;
+
+        public DateTime CurrentTime { get; private set; }
+
+        public bool IsRunning
+        {
+            get => _isRunning;
+
+            set
+            {
+                if (!_isRunning && value)
+                {
+                    _timer.Start();
+                }
+                else if (_isRunning && !value)
+                {
+                    _timer.Stop();
+                }
+
+                _isRunning = value;
+            }
+        }
+
+        public FastClock(DateTime currentTime)
+        {
+            CurrentTime = currentTime;
+            _timer = new DispatcherTimer();
+            _timer.Tick += OnTimerTick;
+            _timer.Interval += TimeSpan.FromMilliseconds(1000);
+        }
+
+        private void OnTimerTick(object sender, EventArgs e)
+        {
+            CurrentTime = CurrentTime.AddMinutes(1);
+            OnOneMinuteIsOver(CurrentTime);
+        }
+
+        protected virtual void OnOneMinuteIsOver(DateTime time)
+        {
+            OneMinuteIsOver?.Invoke(this, time);
+        }
     }
 }
--- source/FastClock/EventsDemo.FastClockWpf/MainWindow.xaml.cs
+++ source/FastClock/EventsDemo.FastClockWpf/MainWindow.xaml.cs
@@ -15,24 +15,34 @@ namespace EventsDemo.FastClockWpf
 
         private void MetroWindow_Initialized(object sender, EventArgs e)
         {
+            FastClock.FastClock clock = FastClock.FastClock.Instance;
+
             DatePickerDate.SelectedDate = DateTime.Today;
             TextBoxTime.Text = DateTime.Now.ToShortTimeString();
+
+            clock.OneMinuteIsOver += FastClockOneMinuteIsOver;
         }
 
         private void ButtonSetTime_Click(object sender, RoutedEventArgs e)
         {
+            SetFastClockStartDateAndTime();
         }
 
         private void SetFastClockStartDateAndTime()
         {
+            TextBlockDate.Text = DatePickerDate.Text;
+            TextBlockTime.Text = TextBoxTime.Text; //stimmt noch nicht
         }
 
         private void FastClockOneMinuteIsOver(object sender, DateTime fastClockTime)
         {
+            TextBlockTime.Text = fastClockTime.ToShortTimeString();
         }
 
         private void CheckBoxClockRuns_Click(object sender, RoutedEventArgs e)
         {
+            FastClock.FastClock clock = FastClock.FastClock.Instance;
+            clock.IsRunning = CheckBoxClockRuns.IsChecked == true;
         }
 
         private void ButtonCreateView_Click(object sender, RoutedEventArgs e)
